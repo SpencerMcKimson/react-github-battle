cddc9e48ad433c022b59f315d03075ad554b6b35
Spencer McKimson <Spencer McKimson>
2017-04-04 18:40:16 -0700

    Fix typo.

--- app/components/Home.js
+++ app/components/Home.js
@@ -7,7 +7,7 @@ var Home = React.createClass({
     render: function () {
         return (
             <div className='jumbotron col-sm-12 text-center' style={transparentBg}>
-                <h1>Guthub Battle</h1>
+                <h1>Github Battle</h1>
                 <p className='lead'>Some fancy motto</p>
                 <Link to='/playerOne'>
                     <button type='button' className='btn btn-lg btn-success'>Get Started</button>
